1d058e1059ee7cb62fc40d07d23f2ee086bbce86
avenezia <alessandro.venezia86@gmail.com>
2016-07-30 15:44:36 +0200

    Adding solution to sherlock_and_array problem

---
+++ algorithms/search/sherlock_and_array.py
@@ -0,0 +1,28 @@
+test_cases = int(raw_input().strip())
+processed_cases = 0
+
+while processed_cases < test_cases:
+    array_length = int(raw_input().strip())
+    array = map(int, raw_input().strip().split(' '))
+    assert len(array) == array_length
+        
+    index_exists = False
+    index = 0
+    left = []
+    right = array[index + 1 :]
+    left_sum = sum(left)
+    right_sum = sum(right)
+    while (index < array_length - 1 or len(array) == 1) and not index_exists:
+        if left_sum == right_sum:
+            index_exists = True
+        else:
+            value = array[index]
+            left.append(value)
+            left_sum += value
+            right_sum -= array[index + 1]
+        index += 1
+    if index_exists:
+        print "YES"
+    else:
+        print "NO"
+    processed_cases += 1
