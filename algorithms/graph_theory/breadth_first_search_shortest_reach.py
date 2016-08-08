bd8fcb209b291e7ce3ffe49792966ae81dca6631
avenezia <avenezia@users.noreply.github.com>
2016-08-08 09:23:28 +0200

    Changes to breadth_first_search_shortest_reach

--- algorithms/graph_theory/breadth_first_search_shortest_reach.py
+++ algorithms/graph_theory/breadth_first_search_shortest_reach.py
@@ -1,22 +1,23 @@
 from collections import defaultdict
+import math
+
+def print_distances(distance_vector):
+    # Convert inf values in -1
+    distance_vector_with_ones = [str(-1) for distance in distance_vector if math.isinf(distance) else str(distance)]
+    print " ".join(distance_vector_with_ones)
 
 def read_graph_data():
     node_number, edge_number = map(int,raw_input().strip().split(' '))
     nodes_adjacency_list = defaultdict(list)
     for edge_index in range(edge_number):
         from_vertex, to_vertex = map(int,raw_input().strip().split(' '))
-        nodes_adjacency_list[from_vertex] = to_vertex
-        nodes_adjacency_list[to_vertex] = from_vertex
+        nodes_adjacency_list[from_vertex].append(to_vertex)
+        nodes_adjacency_list[to_vertex].append(from_vertex)
     starting_vertex = int(raw_input().strip())
     return nodes_adjacency_list, starting_vertex
 
-test_cases = int(raw_input().strip())
-processed_cases = 0
-
-#TODO: to be completed and refactored
-while processed_cases < test_cases:
+def shortest_path(nodes_adjacency_list, starting_vertex):
     edge_weight = 6
-    nodes_adjacency_list, starting_vertex = read_graph_data()
     node_number = len(nodes_adjacency_list.keys())
     # node_number + 1 because the node indexes are given starting from 1.
     visited = [False] * (node_number + 1)
@@ -32,5 +33,13 @@ while processed_cases < test_cases:
             if not visited[adjacent_node]:
                 node_queue.append(adjacent_node)
         visited[current_node] = True
-    processed_cases += 1
+    return distances
 
+test_cases = int(raw_input().strip())
+processed_cases = 0
+
+while processed_cases < test_cases:
+    nodes_adjacency_list, starting_vertex = read_graph_data()
+    distances = shortest_path(nodes_adjacency_list, starting_vertex)
+    print_distances(distances)
+    processed_cases += 1
