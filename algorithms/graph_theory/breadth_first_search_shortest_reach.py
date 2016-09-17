b2b9a1d544c49ef5abbe72698bf7b12e0270a975
avenezia <alessandro.venezia86@gmail.com>
2016-09-17 18:43:15 +0200

    Fixing the output and the order of the adjacency list

--- algorithms/graph_theory/breadth_first_search_shortest_reach.py
+++ algorithms/graph_theory/breadth_first_search_shortest_reach.py
@@ -1,29 +1,35 @@
-from collections import defaultdict
+from collections import OrderedDict
 import math
 
 def print_distances(distance_vector):
-    # Convert inf values in -1
-    distance_vector_with_ones = [str(-1) for distance in distance_vector if math.isinf(distance) else str(distance)]
+    # Actions to match the expected output:
+    # - removing the first element because the graph indexes start from 1.
+    # - removing the starting index (the one with distance 0);
+    # - converting inf values in -1
+    distance_vector = distance_vector[1:]
+    distance_vector.remove(0)
+    distance_vector_with_ones = [str(-1) if math.isinf(distance) else str(distance) for distance in distance_vector]
     print " ".join(distance_vector_with_ones)
 
 def read_graph_data():
     node_number, edge_number = map(int,raw_input().strip().split(' '))
-    nodes_adjacency_list = defaultdict(list)
+    nodes_adjacency_list = OrderedDict()
     for edge_index in range(edge_number):
         from_vertex, to_vertex = map(int,raw_input().strip().split(' '))
-        nodes_adjacency_list[from_vertex].append(to_vertex)
-        nodes_adjacency_list[to_vertex].append(from_vertex)
+        # Adding the edge to the 2 adjacency lists.
+        nodes_adjacency_list.setdefault(from_vertex, []).append(to_vertex)
+        nodes_adjacency_list.setdefault(to_vertex, []).append(from_vertex)
     starting_vertex = int(raw_input().strip())
-    return nodes_adjacency_list, starting_vertex
+    return node_number, nodes_adjacency_list, starting_vertex
 
-def shortest_path(nodes_adjacency_list, starting_vertex):
+def shortest_path(node_number, nodes_adjacency_list, starting_vertex):
     edge_weight = 6
-    node_number = len(nodes_adjacency_list.keys())
     # node_number + 1 because the node indexes are given starting from 1.
     visited = [False] * (node_number + 1)
     distances = [float("inf")] * (node_number + 1)
     node_queue = [starting_vertex]
     distances[starting_vertex] = 0
+    
     while node_queue:
         current_node = node_queue.pop(0)
         adjacent_nodes = nodes_adjacency_list[current_node]
@@ -33,13 +39,13 @@ def shortest_path(nodes_adjacency_list, starting_vertex):
             if not visited[adjacent_node]:
                 node_queue.append(adjacent_node)
         visited[current_node] = True
+
     return distances
 
 test_cases = int(raw_input().strip())
 processed_cases = 0
 
 while processed_cases < test_cases:
-    nodes_adjacency_list, starting_vertex = read_graph_data()
-    distances = shortest_path(nodes_adjacency_list, starting_vertex)
+    distances = shortest_path(*read_graph_data())
     print_distances(distances)
     processed_cases += 1
